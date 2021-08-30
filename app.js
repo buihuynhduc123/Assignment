8e55673d3a42a2c9dd0cc8f12bcf03f84b9313cb
buihuynhduc123 <buihuynhduc96@gmail.com>
2021-08-30 21:51:07 +0700

    ok

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 
 const {ObjectId,MongoClient} = require('mongodb');
 const url = 'mongodb+srv://huyduc:123@cluster0.82mff.mongodb.net/test';
+//const url ='mongodb://localhost:27017';
 
 
 app.set('view engine', 'hbs')
@@ -23,14 +24,14 @@ app.post('/insert', async(req,res) => {
     res.redirect("/");
 })
 
-app.post('/update', async(req,res) => {
+app.post('/update',async(req,res) => {
     const inputName = req.body.txtName;
     const inputPrice = req.body.txtPrice;
     const id = req.body.txtId;
     const inputPicture = req.body.txtPicture;
     const filter = {_id: ObjectId(id)}
     
-    const newValue = { $set: { name: inputName, price: inputPrice, picture: inputPicture} };
+    const newValue = { $set: { name: inputName, price: inputPrice, picture: inputPicture}};
     const client = await MongoClient.connect(url);
     const dbo = client.db("asm2");
     await dbo.collection("products").updateOne(filter, newValue)
@@ -46,12 +47,13 @@ app.get('/delete',async(req,res)=>{
     res.redirect("/");
 })
 
-app.get('/edit', async(req,res) => {
+app.get('/edit',async(req,res) => {
     const id = req.query.id;
+
     const client = await MongoClient.connect(url);
     const dbo = client.db("asm2");
     const p = await dbo.collection("products").findOne({"_id":ObjectId(id)});
-    res.render("edit", { product: p });
+    res.render("edit",{product:p});
 })
 
 app.post('/search', async(req,res) =>{
